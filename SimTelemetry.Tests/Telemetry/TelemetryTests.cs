509347fc8c0eca643bc947fa5268e51e5c59c19e
Hans de Jong <hans1990@gmail.com>
2013-02-04 10:30:07 +0100

    Improved MM timer.

--- SimTelemetry.Tests/Telemetry/TelemetryTests.cs
+++ SimTelemetry.Tests/Telemetry/TelemetryTests.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using NUnit.Framework;
 using SimTelemetry.Domain.Plugins;
+using SimTelemetry.Domain.Utils;
 
 namespace SimTelemetry.Tests.Telemetry
 {
@@ -57,7 +57,7 @@ namespace SimTelemetry.Tests.Telemetry
                 testPlugin.SimulatorStart(rf);
                 telemetryObject = new Domain.Aggregates.Telemetry(testPlugin.TelemetryProvider, rf);
 
-                MultimediaTimer t = new MultimediaTimer(1000);
+                MMTimer t = new MMTimer(1000);
                 t.Tick += (o, s) =>
                                  {
                                      Console.WriteLine(calls);
@@ -66,7 +66,7 @@ namespace SimTelemetry.Tests.Telemetry
                 t.Start();
 
                 Stopwatch w = new Stopwatch();
-                MultimediaTimer mt = new MultimediaTimer(20);
+                MMTimer mt = new MMTimer(20);
                 mt.Tick += (a, b) =>
                                {
                                    w.Stop();
@@ -83,83 +83,5 @@ namespace SimTelemetry.Tests.Telemetry
             }
         }
 
-    }
-
-    public
-    class MultimediaTimer
-    {
-        private delegate void TimerEventHandler(int id, int msg, IntPtr user, int dw1, int dw2);
-        private TimerEventHandler handler;
-
-        public event EventHandler Tick;
-
-        private const int TIME_PERIODIC = 1;
-        private const int EVENT_TYPE = TIME_PERIODIC;
-
-        [DllImport("winmm.dll")]
-        private static extern int timeSetEvent(int delay, int resolution, TimerEventHandler handler, IntPtr user, int eventType);
-
-        [DllImport("winmm.dll")]
-        private static extern int timeKillEvent(int id);
-
-        [DllImport("winmm.dll")]
-        private static extern int timeBeginPeriod(int msec);
-
-        [DllImport("winmm.dll")]
-        private static extern int timeEndPeriod(int msec);
-
-        private int interval = 1;
-        public int Interval
-        {
-            get
-            {
-                return interval;
-            }
-            set
-            {
-                if (value <= 0) return;
-
-                interval = value;
-            }
-        }
-
-        private int timerID = 0;
-
-        public MultimediaTimer(int i)
-        {
-            Interval = i;
-        }
-
-        public void Start()
-        {
-            if (timerID != 0) return;
-
-            timeBeginPeriod(1);
-            handler = new TimerEventHandler(TimerHandler);
-            timerID = timeSetEvent(interval, 1, handler, IntPtr.Zero, EVENT_TYPE);
-        }
-
-        private void TimerHandler(int id, int msg, IntPtr user, int dw1, int dw2)
-        {
-            if (Tick != null)
-                Tick(this, new EventArgs());
-        }
-
-        public void Stop()
-        {
-            if (timerID == 0) return;
-
-            int err = timeKillEvent(timerID);
-            timeEndPeriod(1);
-            timerID = 0;
-        }
-
-        public bool Enabled
-        {
-            get
-            {
-                return timerID != 0;
-            }
-        }
     }
 }
